0955a2834334fcf7212e6509936a32742a657a08
kevinkalvin62 <kevinkalvin62@gmail.com>
2025-04-22 22:41:17 -0600

    no lop se

--- index.js
+++ index.js
@@ -30,13 +30,6 @@ db.connect((err) => {
 
 
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error al conectar a la base de datos: ' + err.stack);
-    return;
-  }
-  console.log('Conectado a la base de datos con ID ' + connection.threadId);
-});
 
 
 db.connect(err => {
