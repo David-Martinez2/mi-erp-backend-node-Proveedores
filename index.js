59def9102d26106be8f8c00b45fc678d54ea1572
kevinkalvin62 <kevinkalvin62@gmail.com>
2025-04-22 22:26:41 -0600

    corregir

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ const moment = require('moment');
 
 // Configuración de la base de datos
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'sqkyBuvVFVzzDLrJhBOQdIRXHeCRmcCk',
-  database: 'mi_erp_db'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
+
 
 db.connect(err => {
   if (err) {
